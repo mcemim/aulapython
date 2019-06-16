1a9fea42ed0dd57bc17b8c7d23ac220e1dcda8fa
mcemim <42742457+mcemim@users.noreply.github.com>
2019-06-16 20:09:14 -0300

    Delete trabalho.py

--- trabalho.py
+++
@@ -1,8 +0,0 @@
-import pandas
-url = 'https://raw.githubusercontent.com/mcemim/aulapython/master/tabela3939.csv'
-data = pandas.read_csv(url, sep=";", header=4,
-                       names=["Nível","Cód.","Regiao","2011","2012","2013","2014","2015","2016","2017"])
-print(data)
-data.head()
-
-data.tail()
